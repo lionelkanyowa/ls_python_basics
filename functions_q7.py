7ad2539c9ee7c73e4767f2632c0bfc4a01041302
Lionel Kanyowa <lionelkanyowa@gmail.com>
2025-01-19 15:09:25 -0500

    chore: Saving a file

--- functions_q7.py
+++ functions_q7.py
@@ -5,11 +5,3 @@ def string_transform(string):
     return string.replace('Ruby', 'Python')
 
 print(string_transform('Captain Ruby'))
-
-given_string = 'Captain Ruby'
-replace_string = "Python"
-
-def strip_change_string(string, replace_string):
-    return(f'{given_string[0:given_string.index(' ')+1]}{replace_string}')
-
-print(strip_change_string(given_string, replace_string))
